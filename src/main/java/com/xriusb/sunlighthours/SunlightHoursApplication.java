1c88252a215975a5d5c00143d87a9ea351360e5b
xriusb <xaviriusb@hotmai.com>
2020-09-02 19:52:09 +0200

    create Spring Boot base app

---
+++ src/main/java/com/xriusb/sunlighthours/SunlightHoursApplication.java
@@ -0,0 +1,13 @@
+package com.xriusb.sunlighthours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunlightHoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunlightHoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xriusb/sunlighthours/SunlightHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xriusb.sunlighthours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlightHoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
